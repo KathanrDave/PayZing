9a6dbad3b3fa9f897cb0bf4395a235bb4e26d3ee
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-03-02 17:10:41 +0530

    Deployed Smart Contract

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,8 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-verify");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.9",
+  networks:{
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.API_KEY,
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  }
 };

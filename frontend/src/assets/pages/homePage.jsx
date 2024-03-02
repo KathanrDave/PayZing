274ee38479fc86970f76d424c2465d08d7b8cac2
kathan07 <thakkarkathan1904@gmail.com>
2024-03-03 02:29:57 +0530

    navbar component

--- frontend/src/assets/pages/homePage.jsx
+++ frontend/src/assets/pages/homePage.jsx
@@ -3,6 +3,8 @@ import AccountDetails from "../component/accountDetails";
 import CurrentBalance from "../component/currentBalance";
 import TableDetails from "../component/tableDetails";
 import PayButtons from "../component/payButtons";
+import Navbar from "../component/Navbar";
+
 const HomePage = () => {
   const address = "0x123.....";
   const name = "Divykumar";
@@ -11,25 +13,28 @@ const HomePage = () => {
   const history = "";
 
   return (
-    <div className="flex h-full  bg-[url('/public/herobg.png')] bg-no-repeat h-full">
+    <>
+      <Navbar/>
+      <div className="flex h-full  bg-[url('/public/herobg.png')] bg-no-repeat">
         <div className="flex w-full">
-        <div className="flex-start  w-1/3 mt-3 ml-3">
-        <div className="flex-col items-center">
-          <AccountDetails address={address} name={name} balance={balance} />
-        </div>
-        <div className="flex">
-          <PayButtons />
-        </div>
-        <div>
-          <CurrentBalance dollars={dollars} />
+          <div className="flex-start  w-1/3 mt-3 ml-3">
+            <div className="flex-col items-center">
+              <AccountDetails address={address} name={name} balance={balance} />
+            </div>
+            <div className="flex">
+              <PayButtons />
+            </div>
+            <div>
+              <CurrentBalance dollars={dollars} />
+            </div>
+          </div>
+          <div className="w-2/3 mt-6 ml-3 mr-3 rounded">
+            <TableDetails />
+          </div>
         </div>
       </div>
-      <div className="w-2/3 mt-6 ml-3 mr-3 rounded">
-      <TableDetails/>
-        </div>
-      </div>
-  
-    </div>
+    </>
+
   );
 };
 
